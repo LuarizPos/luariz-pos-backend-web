c74a8181bde1cb2dbb6336bbbebfe86c93e4a7e7
Prayugo Dwi <pray.arsian24@gmail.com>
2021-03-05 23:44:59 +0700

    update manage

--- app/manage.py
+++ app/manage.py
@@ -40,10 +40,10 @@ app.config['IMAGE_FOLDER'] = picFolder
 app.config['IMAGE_FOLDER_TEMPLATE'] = picFolderTempate
 
 app.config['MAIL_SERVER']= 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
+app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
 db = SQLAlchemy(app)
