11a9a85bc473cbc7c31ca9ecee357de1d2f3e9da
Prayugo Dwi <pray.arsian24@gmail.com>
2021-03-06 00:46:08 +0700

    manage

--- app/manage.py
+++ app/manage.py
@@ -43,8 +43,8 @@ app.config['MAIL_SERVER']= 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', False)
-app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', True)
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
